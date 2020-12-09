247376791a633e71a792801f003e1e65c7cadba9
Brian Parma <execrable@gmail.com>
2020-12-09 16:55:35 -0700

    clean up d9

--- aoc/2020/d9.py
+++ aoc/2020/d9.py
@@ -2,25 +2,26 @@ from itertools import combinations
 
 from aocd import data
 
+
+def part1(nums):
+    for idx, x in enumerate(nums[25:]):
+        if not any(a + b == x for (a, b) in combinations(nums[idx:idx + 25], 2)):
+            return x
+
+
+def part2(x, nums):
+    for i in range(len(nums)):
+        for j in range(i + 2, len(nums)):
+            if s := sum(pre := nums[i:j]) == x:
+                return min(pre) + max(pre)
+            elif s > x:
+                break
+
+
 nums = tuple(map(int, data.splitlines()))
 
+# 133015568
+print(f'part1: {(val := part1(nums))}')
 
-def check(x, pre):
-    return any(a + b == x for (a, b) in combinations(pre, 2))
-
-
-for idx, x in enumerate(nums[25:]):
-    if not check(x, nums[idx:idx + 25]):
-        # 133015568
-        print(f'part1: {x} (i={idx})')
-        break
-else:
-    print('no hit')
-
-for i in range(len(nums)):
-    for j in range(i + 2, len(nums)):
-        if s := sum(nums[i:j]) == x:
-            # 16107959
-            print(f'part2: {min(nums[i:j]) + max(nums[i:j])}')
-        elif s > x:
-            break
+# 16107959
+print(f'part2: {part2(val, nums)}')

125bb8afd6004a38f24b282f7b11e79915dd9a1b
Brian Parma <brian_parma@partech.com>
2020-12-15 11:03:46 -0700

    add d10 part 2

--- aoc/2020/d10.py
+++ aoc/2020/d10.py
@@ -1,4 +1,5 @@
 from collections import Counter
+from time import perf_counter
 
 from aocd import data
 
@@ -8,5 +9,15 @@ dJ = Counter(jolts[i] - jolts[i - 1] for i in range(1, len(jolts)))
 
 # 2080
 print(f'part1: {dJ[1] * dJ[3]}')
-# submit(dJ[1] * dJ[3], reopen=False)
 
+t = perf_counter()
+
+tot = Counter({0: 1})
+for jolt in jolts:
+    for i in range(1, 4):
+        tot[jolt + i] += tot[jolt]
+
+# 6908379398144
+print(f'part2: {tot[jolts[-1]]}')
+
+print(f'{perf_counter() - t:.2f}s')

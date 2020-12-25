651e225bffdb870cd17f6483bbf66b4dfac1e681
Brian Parma <execrable@gmail.com>
2020-12-24 23:17:17 -0700

    finish d23

--- aoc/2020/d23.py
+++ aoc/2020/d23.py
@@ -1,69 +1,42 @@
+from itertools import accumulate, islice
+from math import prod
+
 from aocd import data
 
 from aoc.util import perf
 
-data = '389125467'
-
 cups = [int(d) for d in data]
 N = max(cups)
 
 
-def rot(n):
+def rot(n, N):
     return ((n - 1) % N) + 1
 
 
-for move in range(100):
-    cur, *rest = cups
-    held, rest = rest[:3], rest[3:]
-    next = rot(cur - 1)
-    while next in held:
-        next = rot(next - 1)
-    i = rest.index(next) + 1
-    cups = rest[:i] + held + rest[i:] + [cur]
+@perf
+def run(cups, moves):
+    N = len(cups)
+    cur = cups[0]
+    after = {c: cups[(i + 1) % N] for i, c in enumerate(cups)}
+    for _ in range(moves):
+        held = tuple(accumulate([after[cur], 0, 0], lambda r, _: after[r]))
+        t = rot(cur - 1, N)
+        while t in held:
+            t = rot(t - 1, N)
+        after[cur] = after[held[-1]]
+        after[held[-1]] = after[t]
+        after[t] = held[0]
+        cur = after[cur]
+    return accumulate([1] * N, lambda r, _: after[r])
 
-i = cups.index(1)
 
 # 65432978
-print(f'part1: {"".join(str(x) for x in (cups[i:] + cups[:i]))[1:]}')
+print(f'part1: {"".join(str(x) for x in run(cups, 100))[1:]}')
 
-cups = [int(d) for d in data]
-N = max(cups)
-M = 10
+M = 1_000_000
 cups += list(range(N + 1, M + 1))
 N = M
 
-
-# cups = [999_998, 999_999, 1_000_000, 8, 9,
-#         *[i for i in range(11, 999_998 + 1) if i % 4 != 2],
-#         1, 3, 4, 6, 7, 2, 5,
-#         *[i for i in range(10, 999_994 + 1, 4)]]
-
-# idx = cups.index(14)
-
-@perf
-def part2(cups):
-    print(0, cups[:12], cups[-25:])
-    last = None
-    for move in range(M * 10):
-        cur, *rest = cups
-        held, rest = rest[:3], rest[3:]
-        next = rot(cur - 1)
-        while next in held:
-            next = rot(next - 1)
-        i = rest.index(next) + 1
-        cups = rest[:i] + held + rest[i:] + [cur]
-        # if (move + 1) % 100 == 0:
-        # print(move + 1, cups[:12], cups[-25:])
-        # print('  ',cups[idx-18:idx+4])
-        i = cups.index(1)
-        # if cups[i:i + 3] == [1, 524,822]:
-        if cups[i:i+3] != last:
-            print('wtf',move, cups[i:i + 3])
-            last = cups[i:i+3]
-
-    i = cups.index(1)
-    print(cups[i:i + 10])
-
-
-part2(cups)
-# print(cups)
+r = run(cups, N * 10)
+# 287230227046
+print(f'part2: {prod(islice(r, 1, 3))}')

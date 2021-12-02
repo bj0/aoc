5e6ca60d5324929e2bcb9705c2136a7acbcf9a9e
Brian Parma <execrable@gmail.com>
2021-12-02 09:50:29 -0700

    clean up

--- aoc/2020/d1.py
+++ aoc/2020/d1.py
@@ -1,19 +1,13 @@
 from itertools import combinations
+from math import prod
 
 from aocd import data
 
 nums = [int(x) for x in data.strip().split()]
 
 
-# nums = [int(x) for x in """1721
-# 979
-# 366
-# 299
-# 675
-# 1456""".strip().split()]
-
-
 def part1(nums):
+    """find pair that sums to 2020"""
     for i, a in enumerate(nums):
         for b in nums[i + 1:]:
             if a + b == 2020:
@@ -27,6 +21,7 @@ print(f'part1 a={a},b={b}, {a + b}, {a * b}')
 
 
 def part2(nums):
+    """find triplet that sums to 2020"""
     for i, a in enumerate(nums):
         for j, b in enumerate(nums[i + 1:]):
             for c in nums[i + j + 1:]:
@@ -39,8 +34,10 @@ a, b, c = part2(nums)
 
 print(f'part2 a={a}, b={b}, c={c}, {a + b + c}, {a * b * c}')
 
-# shorter
+# shorter version
 nums = [int(x) for x in data.strip().split()]
-p1 = next(a * b for (a, b) in combinations(nums, 2) if a + b == 2020)
-p2 = next(a * b * c for (a, b, c) in combinations(nums, 3) if a + b + c == 2020)
+# p1 = next(a * b for (a, b) in combinations(nums, 2) if a + b == 2020)
+# p2 = next(a * b * c for (a, b, c) in combinations(nums, 3) if a + b + c == 2020)
+p1 = next(prod(c) for c in combinations(nums, 2) if sum(c) == 2020)
+p2 = next(prod(c) for c in combinations(nums, 3) if sum(c) == 2020)
 print(f'p1={p1}, p2={p2}')

f2e79c51198711ef70d8c93243ac1a02fdb4d11a
Brian Parma <brian_parma@partech.com>
2021-12-06 16:42:51 -0700

    add cleaner day 4

--- aoc/2021/d4.py
+++ aoc/2021/d4.py
@@ -1,59 +1,68 @@
-import re
-
 from aocd import data
 
-nums, *boards = data.strip().split('\n\n')
-nums = nums.split(',')
+
+def parse_input(data):
+    """parse input into list of draw numbers and 3d array of boards"""
+    nums, *boards = data.strip().split('\n\n')
+    nums = nums.split(',')
+    boards = [[row.split() for row in board.split("\n")] for board in boards]
+    return nums, boards
 
 
-def check_boards(boards, num):
-    boards = [re.sub(rf'\b{num}\b', '.', b) for b in boards]
+def check_board(nums, board):
+    """check board rows and columns for match"""
+    def check(group):
+        if len(set(group) - set(nums)) == 0:
+            return True
 
-    # check rows
-    for b in boards:
-        if re.search(r'(\. *){5}', b):
-            # print('row')
-            return boards, b
+    for row in board:
+        if check(row):
+            return True
 
-    # check columns
-    for b in boards:
-        items = b.split()
-        for i in range(5):
-            if all(items[i + 5 * j] == '.' for j in range(5)):
-                # print('col')
-                return boards, b
+    for col in zip(*board):
+        if check(col):
+            return True
 
-    return boards, False
+    return False
 
 
-def part1(nums, boards):
-    for num in nums:
-        boards, win = check_boards(boards, num)
-        if win:
-            print(win)
-            if isinstance(win, str):
-                win = win.split()
-            return sum(int(x) for x in win if x != '.') * int(num)
-    return -1
+def drawer(nums):
+    for i in range(len(nums)):
+        yield nums[:i + 1]
 
 
-def part2(nums, boards):
-    for num in nums:
-        print(f'doing {num}')
-        boards, win = check_boards(boards, num)
-        if win:
-            while win:
-                boards.remove(win)
-                last = win
-                boards, win = check_boards(boards, num)
-                print(f'win {len(boards)}')
-            if len(boards) == 0:
-                if isinstance(last, str):
-                    win = last.split()
-                return sum(int(x) for x in win if x != '.') * int(num)
-    return -1
+def matcher(nums, boards):
+    """iterate through matches"""
+    for draw_nums in drawer(nums):
+        for board in boards:
+            if check_board(draw_nums, board):
+                boards.remove(board)
+                yield board, draw_nums
+
+        if len(boards) == 0:
+            break
 
 
-print(f'part1: {part1(nums, boards)}')
+def find_first_match(nums, boards):
+    return next(matcher(nums, boards))
 
-print(f'part2: {part2(nums, boards)}')
+
+def find_last_match(nums, boards):
+    return [m for m in matcher(nums, boards)][-1]
+
+
+def find(nums, boards, matcher):
+    board, nums = matcher(nums, boards)
+
+    return sum(int(x) for row in board for x in row if x not in nums) * int(nums[-1])
+
+
+def main():
+    nums, boards = parse_input(data)
+    print(f'part1: {find(nums, boards, find_first_match)}')
+
+    print(f'part2: {find(nums, boards, find_last_match)}')
+
+
+if __name__ == "__main__":
+    main()

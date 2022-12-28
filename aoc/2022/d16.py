a8e42c74622642a699cac35acbcfe5efed110f63
Brian Parma <execrable@gmail.com>
2022-12-27 19:44:22 -0700

    finish day 16

--- aoc/2022/d16.py
+++ aoc/2022/d16.py
@@ -1,5 +1,6 @@
 from collections import deque
 from functools import cache, lru_cache
+from itertools import combinations
 
 from aocd import data
 
@@ -44,7 +45,7 @@ def walk_paths(node, t, shut, T=30):
         (
             sum
             + max(
-                walk_paths(next, t + 1 + dists[node][next], shut - {next})
+                walk_paths(next, t + 1 + dists[node][next], shut - {next}, T)
                 for next in shut
             )
         )
@@ -63,66 +64,14 @@ def part1(g, dists):
 print(f"part1: {part1(g, dists)}")
 
 
-@lru_cache(100_000)
-# @cache
-def walk_paths2(node0, node1, t0, t1, shut):
-    def first(x):
-        return x[0]
-
-    t = min(t0, t1)
-    suma = sumb = (0, ((90, "end"),))
-    # if we run out of time
-    if t >= 26:
-        return suma
-    # if we're done opening something
-    if t == t0:
-        sum = g.nodes[node0]["rate"] * (26 - t)
-        # more valves to open
-        if shut:
-            mx = max(
-                (
-                    walk_paths2(
-                        next, node1, t + 1 + dists[node0][next], t1, shut - {next}
-                    )
-                    for next in shut
-                ),
-                key=first,
-            )
-            suma = (mx[0] + sum, ((t, f"a:{node0}"),) + mx[1])
-        # no more valves to open, elephant still opening one?
-        elif t1 > t:
-            if node1 == "end":
-                return (sum, ((f"b:{node0}"),))
-            mx = walk_paths2("end", node1, 50, t1, shut)
-            return (mx[0] + sum, ((t, f"a:{node0}"),) + mx[1])
-    # if elephant's done opening something
-    if t == t1:
-        sum = g.nodes[node1]["rate"] * (26 - t)
-        # more valvues to open
-        if shut:
-            mx = max(
-                (
-                    walk_paths2(
-                        node0, next, t0, t + 1 + dists[node1][next], shut - {next}
-                    )
-                    for next in shut
-                ),
-                key=first,
-            )
-            sumb = (mx[0] + sum, ((t, f"b:{node1}"),) + mx[1])
-        # no more valves to open, we still opening one?
-        elif t0 > t:
-            if node0 == "end":
-                return (sum, ((f"b:{node1}"),))
-            mx = walk_paths2(node0, "end", t0, 50, shut)
-            return (mx[0] + sum, ((t, f"b:{node1}"),) + mx[1])
-    return max(suma, sumb, key=first)
-
-
 @perf
 def part2(g, dists):
-    return walk_paths2(
-        "AA", "AA", 0, 0, frozenset(n for n, d in g.nodes(data=True) if d["rate"] > 0)
+    nodes = frozenset((n for n, d in g.nodes(data=True) if d["rate"] > 0))
+    return max(
+        walk_paths("AA", 0, frozenset(valves), T=26)
+        + walk_paths("AA", 0, nodes - set(valves), T=26)
+        for i in range(2, len(g.nodes) // 2)
+        for valves in combinations(nodes, i)
     )
 
 

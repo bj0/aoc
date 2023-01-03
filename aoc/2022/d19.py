e4da5442d471d7956c32be9a05f35cdfbb4a0317
Brian Parma <execrable@gmail.com>
2023-01-03 09:21:00 -0700

    finish day 18

---
+++ aoc/2022/d19.py
@@ -0,0 +1,41 @@
+from collections import deque
+from functools import cache, lru_cache
+
+import networkx as nx
+import numpy as np
+from aocd import data
+import numpy as np
+from aoc.util import perf
+
+
+# data = """2,2,2
+# 1,2,2
+# 3,2,2
+# 2,1,2
+# 2,3,2
+# 2,2,1
+# 2,2,3
+# 2,2,4
+# 2,2,6
+# 1,2,5
+# 3,2,5
+# 2,1,5
+# 2,3,5"""
+
+
+@perf
+def part1():
+
+
+#
+print(f"part1: {part1()}")
+
+
+
+
+@perf
+def part2():
+
+
+# 2606
+print(f"part2: {part2()}")
